449acac58e43442f028c2c6b3a875087e4a73b90
Hamidou Woury Ba <hamidouwouryba13@gmail.com>
2025-04-15 17:37:23 +0000

    Initialisation et mise en place des enttités

---
+++ book-network/src/main/java/com/hamidou/book/role/Role.java
@@ -0,0 +1,40 @@
+package com.hamidou.book.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hamidou.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    private Integer id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> user;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+}
